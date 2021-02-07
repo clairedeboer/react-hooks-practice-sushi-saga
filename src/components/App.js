7588b9309b84d5ac2eab2dd772cbbf762e9d7889
clairedeboer <claire.deboer@gmail.com>
2021-02-06 22:31:28 -0500

    commit tree a12aaa62e8929d05c152799b16e9de967e86820c

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+  const [sushiList, setSushiList] = useState([])
+  const [displaySushi, setDisplaySushi] = useState([])
+  const [plates, setPlates] = useState([])
+  const [remainingMoney, setRemainingMoney] = useState(60)
+  const [index, setIndex] = useState(0)
+
+  // const [displayFour, setDisplayFour] = useState(sushiList.slice(0, 4))
+const onEatSushi = () => {
+  setPlates([...plates, 1])
+}
+
+const onChangeTotal = (price) => {
+  setRemainingMoney(remainingMoney - price)
+}
+
+const showMoreSushi = () => {
+  setIndex((index) => index + 4)
+  const newIndex = index + 4
+  setDisplaySushi(sushiList.slice(newIndex, newIndex + 4))
+  console.log(sushiList)
+}
+
+  useEffect(() => {
+    fetch(API)
+      .then((r) => r.json())
+      .then((sushiData) => {
+        setSushiList(sushiData)
+        setDisplaySushi(sushiData.slice(0, index + 4))});
+  }, []); 
+  
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer sushiList={displaySushi} onShowMoreSushi={showMoreSushi} onEatSushi={onEatSushi} onChangeTotal={onChangeTotal} remainingMoney={remainingMoney}/>
+      <Table plates={plates} remainingMoney={remainingMoney}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,32 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({
+  sushiList,
+  onEatSushi,
+  onChangeTotal,
+  remainingMoney,
+  onShowMoreSushi
+}) {
+  const sushiComponents = sushiList.map((sushi) => {
+    return (
+      <Sushi
+        key={sushi.id}
+        onEatSushi={onEatSushi}
+        onChangeTotal={onChangeTotal}
+        remainingMoney={remainingMoney}
+        image={sushi.img_url}
+        name={sushi.name}
+        price={sushi.price}
+      />
+    );
+  });
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiComponents}
+      <MoreButton onShowMoreSushi={onShowMoreSushi} />
     </div>
   );
 }
